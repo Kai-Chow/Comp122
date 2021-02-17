87d204c9fa845b876d417d20cecfc37842783a92
Thomas <78735204+Kai-Chow@users.noreply.github.com>
2021-02-17 18:29:37 +0000

    Update SimpleCircle.java

--- Week 2 Labs/SimpleCircle.java
+++ Week 2 Labs/SimpleCircle.java
@@ -6,8 +6,10 @@ public class SimpleCircle {
         double r = 2.5;
         
         // replace the following two lines
+        // area = Pi * r^2
+        // circumference = Pi * (r * 2) or d
         double area = Math.PI * (Math.pow(r, 2));
-        double circumference = Math.PI * r;
+        double circumference = Math.PI * (r * 2);
   
         System.out.println(r);
         System.out.println(area);
