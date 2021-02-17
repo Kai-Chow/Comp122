09ea74a1bb241ce99b3f30fade715f418643100d
Thomas <78735204+Kai-Chow@users.noreply.github.com>
2021-02-17 18:01:23 +0000

    Create Pythagoras.java

---
+++ Week 2 Labs/Pythagoras.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+
+public class Pythagoras {
+	public static void main(String[] args) {
+        double a = 3.0;
+        double b = 5.0;
+        double c;
+        
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+        System.out.println(c);
+    }    
+	// c = 5.83 (rounded)
+}
