61b5c8b25e914b7c88cd58e2c7ae464e1440bf14
Thomas <78735204+Kai-Chow@users.noreply.github.com>
2021-02-17 17:59:19 +0000

    Create FPTestApp.java

---
+++ Week 2 Labs/FPTestApp.java
@@ -0,0 +1,10 @@
+
+public class FPTestApp {
+	public static void main (String args[]) {
+        double x = 0.05;
+        double y = x * 20 ; 
+        
+        System.out.println(y);
+     }
+	// I still get an answer of 0.9 recurring
+}
